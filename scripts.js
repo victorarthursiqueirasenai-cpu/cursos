7fa86662d143046e700971d76a3c01fd04045b16
victorarthursiqueirasenai@gmail.com <victorarthursiqueirasenai@gmail.com>
2026-03-30 11:33:39 -0300

    git commit -m "sua mensagem aqui"

--- scripts.js
+++ scripts.js
@@ -1,15 +1,31 @@
-const prevButton = document.getAnimationsbyId('prev');
-const nextButton = document.getAnimationsbyId('next');
-const items = document.querySelectorAll('.item');
-const dost =  document.querySelectorAll('.dot');
-const numberintdicator = document.querySelector('.number');
-const list = document.querySelector('.list');
+const prevButton = document.getElementById('prev')
+const nextButton = document.getElementById('next')
+const items = document.querySelectorAll('.item')
+const dots = document.querySelectorAll('.dot')
+const numberIndicator = document.querySelectorAll('.numbers')
+const list = document.querySelectorAll('.list') 
 
 
 let active = 0;
-const total = items.length;
+const total = items.length
 let timer;
 
-prevButton.addEventListener('click', function(){
-    update()
+
+function update(direction){
+
+    document.querySelector('.item.active').classList.remove('active')
+    document.querySelector('.dot.active').classList.remove('active')
+
 }
+
+
+
+
+
+prevButton.addEventListener('click', ()=>{
+    update(-1)
+})
+
+nextButton.addEventListener('click', () => {
+    update(+1)
+})
