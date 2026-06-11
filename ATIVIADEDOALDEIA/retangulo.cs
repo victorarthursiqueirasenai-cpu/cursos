4d7a5b54ca10b7c94126bb9162ddf1ab32f3ca9f
darksanji <victorarthursiqueirasenai@gmail.com>
2026-06-11 11:30:59 -0300

    Create retangulo.cs

---
+++ ATIVIADEDOALDEIA/retangulo.cs
@@ -0,0 +1,58 @@
+using System;using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto2
+{
+    internal class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+        public double Perimetro()
+        { 
+            return 2*(Largura + Altura);
+        }
+        public double Diagonal()
+        { 
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+
+    }
+}
+
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto2
+{
+    internal class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+        public double Perimetro()
+        { 
+            return 2*(Largura + Altura);
+        }
+        public double Diagonal()
+        { 
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+
+    }
+}
